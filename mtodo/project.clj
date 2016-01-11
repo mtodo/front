c230ac2e77c5bc91fa148526af5ca990113c499c
Oleksii Fedorov <waterlink000@gmail.com>
2016-01-11 12:20:20 +0100

    Fix license property in project.clj

--- mtodo/project.clj
+++ mtodo/project.clj
@@ -1,8 +1,8 @@
 (defproject mtodo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://raw.githubusercontent.com/mtodo/front/master/LICENSE"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring-server "0.4.0"]
