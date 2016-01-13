b5606262dd24ff1984b21a8236d7fe33b98d930d
Oleksii Fedorov <waterlink000@gmail.com>
2016-01-13 15:49:29 +0100

    Extract test helper

---
+++ mtodo/test/cljs/mtodo/test_helper.cljs
@@ -0,0 +1,29 @@
+(ns mtodo.test-helper
+    (:require [reagent.core :as reagent]))
+
+(def is-client (not (nil? (try (.-document js/window)
+                              (catch js/Object e nil)))))
+
+(def rflush reagent/flush)
+
+(defn add-test-div [name]
+  (let [doc     js/document
+        body    (.-body js/document)
+        div     (.createElement doc "div")]
+    (.appendChild body div)
+    div))
+
+(defn with-mounted-component [comp f]
+  (when is-client
+    (let [div (add-test-div "_testreagent")]
+      (let [comp (reagent/render-component comp div #(f comp div))]
+        (reagent/unmount-component-at-node div)
+        (rflush)
+        (.removeChild (.-body js/document) div)))))
+
+(defn found-in [re div]
+  (let [res (.-innerHTML div)]
+    (if (re-find re res)
+      true
+      (do (println "Not found: " res)
+          false))))
